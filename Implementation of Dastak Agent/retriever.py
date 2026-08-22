9062bb3eff647f544a456c7caff1d1fe4def8daf
abdulsamad-codes <abdul.samad538541@gmail.com>
2026-08-22 22:57:13 +0500

    ran after a long time

--- Implementation of Dastak Agent/retriever.py
+++ Implementation of Dastak Agent/retriever.py
@@ -3,9 +3,7 @@ from langchain_chroma import Chroma
 from langchain.tools import tool
 from typing import Optional
 
-
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
 vectorstore = Chroma(
     persist_directory="./chroma_db",
     embedding_function=embeddings,
